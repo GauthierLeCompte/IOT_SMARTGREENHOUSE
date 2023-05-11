09ceb6a5931e404fbcb58f6b2d985b42ab494b5c
GauthierLeCompte <gauthier.lecompte@student.uantwerpen.be>
2023-05-11 20:47:41 +0200

    app.py en app.vue adjustments

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-import uuid
 
-# Create app from flask
+# Create a Flask application and enable CORS
 app = Flask(__name__)
+CORS(app)
 
-# To update app constantly
-app.config.from_object(__name__)
+# Store the greenhouse status as a Python dictionary
+greenhouse_status = {
+    "temperature": 20,
+    "lights": False,
+    "humidity": 50,
+    "sprinklers": False
+}
 
-CORS(app, resources={r"/*": {'origins': '*'}})
-
-@app.route('/', methods=['GET'])
-def greetings():
-    return "Hello wor;d"
-
-@app.route('/homepage', methods=['GET'])
-def homepage():
-    return ("homepage")
+# Define the GET route for retrieving the greenhouse status
+@app.route('/api/status', methods=['GET'])
+def get_status():
+    return jsonify(greenhouse_status)
 
 
-# Press the green button in the gutter to run the script.
+# Define the POST route for updating the greenhouse status
+@app.route('/api/update', methods=['POST'])
+def update_status():
+    # Get the new desired status from the request JSON
+    desired_status = request.get_json()
+
+    # Update the greenhouse status with the new desired status
+    for key in desired_status:
+        greenhouse_status[key] = desired_status[key]
+
+    return "Updated", 200
+
+# Run the Flask application on port 5000
 if __name__ == '__main__':
-    app.run(debug=True)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# Front end
-# - npm install -g @vue/cli
-# - npm install --save-dev eslint eslint-plugin-vue
+    app.run(port=5000, debug=True)
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,62 @@
 <template>
   <div id="app">
-
-    <router-view/>
+    <h1>Greenhouse Dashboard</h1>
+    <!-- Input fields for desired temperature, lights, humidity, and sprinklers status -->
+    <div>
+      <label for="temperature">Temperature:</label>
+      <input type="number" id="temperature" v-model.number="desiredStatus.temperature" />
+    </div>
+    <div>
+      <label for="lights">Lights:</label>
+      <input type="checkbox" id="lights" v-model="desiredStatus.lights" />
+    </div>
+    <div>
+      <label for="humidity">Humidity:</label>
+      <input type="number" id="humidity" v-model.number="desiredStatus.humidity" />
+    </div>
+    <div>
+      <label for="sprinklers">Sprinklers:</label>
+      <input type="checkbox" id="sprinklers" v-model="desiredStatus.sprinklers" />
+    </div>
+    <!-- Button for updating the greenhouse status -->
+    <button @click="updateStatus">Update</button>
+    <!-- Display the current greenhouse status -->
+    <h2>Current Greenhouse Status</h2>
+    <pre>{{ greenhouseStatus }}</pre>
   </div>
 </template>
 
-<style>
-#app {
-  margin-top: 60px;
-}
-</style>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      greenhouseStatus: {},
+      desiredStatus: {
+        temperature: 0,
+        lights: false,
+        humidity: 0,
+        sprinklers: false
+      }
+    };
+  },
+  methods: {
+    // Fetch the current greenhouse status from the Flask backend
+    async getStatus() {
+      const response = await axios.get('http://127.0.0.1:5000/api/status');
+      this.greenhouseStatus = response.data;
+    },
+    // Update the greenhouse status in the Flask backend
+    async updateStatus() {
+      console.log("Updating status with:", this.desiredStatus);
+      await axios.post('http://127.0.0.1:5000/api/update', this.desiredStatus);
+      this.getStatus();
+    }
+  },
+  // Used to fetch initial status when component is created
+  created() {
+    this.getStatus();
+  }
+};
+</script>
