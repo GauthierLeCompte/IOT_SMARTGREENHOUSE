7cbca7ba627f1122bd0a9fdd8ef0b3be2df79de5
GauthierLeCompte <gauthier.lecompte@student.uantwerpen.be>
2023-05-24 16:54:17 +0200

    backend push / merge

--- api/src/api.py
+++ api/src/api.py
@@ -17,10 +17,10 @@ BELOW IS AN EXAMPLE FUNCTION FOR LATER REFERENCE @GAUTHIER
 """
 @api_bl.route('/api/predict', methods=['GET', 'POST'])
 def request_prediction():
-
+    print("in dataaaaa")
     data = {'time': 459999,'temp': 21.5, 'outside_temp': 16.2, 'humidity': 51.8, 'lamp_status': 1, 'rain': 0} # example
 
     r = requests.post(url="http://iot_model/model/predict", data=data).json()
 
-    response={"success": True, 'prediction': r}
-    return jsonify(response), 200
+    response = {"success": True, 'prediction': r}
+    return jsonify(data), 200
--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 import requests
+import json
+from datetime import datetime
+import base64
+
+
+#=======================================================================================================================
+#                                              FLASK INITIALIZATION AND SETUP
+#=======================================================================================================================
 
 # Create a Flask application and enable CORS
 app = Flask(__name__)
 CORS(app)
+
+# Register the Blueprint from your api.py
+#app.register_blueprint(api_bl)
 
 # Store the greenhouse status as a Python dictionary
 greenhouse_status = {
@@ -14,11 +25,15 @@ greenhouse_status = {
     "sprinklers": False
 }
 
+
+#=======================================================================================================================
+#                                                       API CALLS
+#=======================================================================================================================
+
 # Define the GET route for retrieving the greenhouse status
 @app.route('/api/status', methods=['GET'])
 def get_status():
     return jsonify(greenhouse_status)
-
 
 # Define the POST route for updating the greenhouse status
 @app.route('/api/update', methods=['POST'])
@@ -32,16 +47,44 @@ def update_status():
 
     return "Updated", 200
 
+# Get prediction from model
+@app.route('/api/get-prediction', methods=['GET'])
+def get_prediction():
+    # Make a request to the /api/predict endpoint
+    response = requests.get('http://127.0.0.1:5001/api/predict')
+    print(response)
 
-@app.route('/app-endpoint', methods=['GET', 'POST'])
-def app_endpoint():
-    # This code will run when a user makes a GET or POST request to "/app-endpoint"
-    response = requests.get('http://<api-server>/api/predict')
+    # If the request was successful, return the prediction
     if response.status_code == 200:
-        return jsonify(response.json()), 200
+        return jsonify(response.json())
     else:
-        return jsonify({"error": "Unable to get prediction"}), response.status_code
+        return Response("Error when getting prediction", status=response.status_code)
 
+
+#=======================================================================================================================
+#                                                CONNECTION TO THE CLOUD
+#=======================================================================================================================
+
+base_url = "https://eu1.cloud.thethings.network/api/v3/as/applications/smart-greenhouse-ntg/packages/storage"
+headers = {
+    "Authorization": "Bearer NNSXS.DWSHILY54MRI6PDGLCUL3YK7A3JJFKHLGCN55XQ.HPQPPV2P3KKHKBUY3KUAAPLEJXK65GSRQZRLYAYFWGZG3RQNQ3NA",
+    "Content-Type": "application/json",
+}
+
+def get_device_data(device_id, data_type="uplink_message"):
+    url = f"{base_url}/devices/{device_id}/packages/storage/{data_type}"
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def get_application_data(data_type="uplink_message"):
+    url = f"{base_url}/{data_type}"
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+#=======================================================================================================================
+#                                                    RUN BACKEND
+#=======================================================================================================================
 
 # Run the Flask application on port 5000
 if __name__ == '__main__':
